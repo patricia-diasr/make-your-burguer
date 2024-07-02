521bf6b5a29435c00cd60609b9dc8b3411ef8cb1
patricia-diasr <patricia.d.rodrigues345@gmail.com>
2024-07-02 10:54:48 -0300

    Project created

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,17 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view/>
+    <Navbar />
+    <router-view />
+    <Footer />
 </template>
+<script>
+import Navbar from "./components/Navbar.vue";
+import Footer from "./components/Footer.vue";
 
-<style lang="less">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+export default {
+    components: {
+        Navbar,
+        Footer,
+    },
+};
+</script>
+<style lang="less"></style>
